358176119e9314ec94db10278b8285f821d122ce
Agata Witczak <witczakagata123@gmail.com>
2023-07-31 22:08:01 +0200

    Finished task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+
+const list = document.querySelectorAll('[data-url]');
+list.forEach(function (el) {
+    const attr = el.getAttribute('data-url');
+    el.setAttribute('href', attr)
+    // const dataset = el.dataset.url
+    // el.setAttribute = ('href', dataset);
+})
