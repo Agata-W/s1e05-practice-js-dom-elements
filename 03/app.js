75cf16c1ffeae213db07bb1abeee36b765253f74
Agata Witczak <witczakagata123@gmail.com>
2023-08-03 23:09:12 +0200

    Finished task 03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,33 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parentElement = document.querySelector('.parent-for-button')
+if (parentElement) {
+    const buttonElement = document.createElement('button')
+
+    // buttonElement.classList.add(buttonSettings.attr.className)
+    // buttonElement.title = buttonSettings.attr.title
+
+    // buttonElement.style.border = buttonSettings.css.border
+    // buttonElement.style.padding = buttonSettings.css.padding
+    // buttonElement.style.color = buttonSettings.css.color
+
+
+    for (const key in buttonSettings.attr) {
+        const value1 = buttonSettings.attr[key]
+        buttonElement[key] = value1
+    }
+    for (const key in buttonSettings.css) {
+        const value2 = buttonSettings.css[key]
+        buttonElement.style[key] = value2
+    }
+
+    buttonElement.innerText = buttonSettings.text
+
+
+
+    parentElement.appendChild(buttonElement)
+}
+
+
